378a3abc572f920635ad132ffee7dd0fcb6ae8a0
Git <nico@hipermediaz.com>
2018-03-16 14:44:18 +0100

    Added Author Manifiest

---
+++ atarialauncher/AuthorManifiest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atarialauncher
+{
+    class AuthorManifiest
+    {
+        public class AssemblyAuthorAttribute : Attribute
+        {
+            private string _author;
+
+            public string Author
+            {
+                get { return _author; }
+                set { _author = value; }
+            }
+
+
+            public AssemblyAuthorAttribute(string author)
+            {
+                _author = author;
+            }
+
+        }
+    }
+}
--- atarialauncher/Properties/AssemblyInfo.cs
+++ atarialauncher/Properties/AssemblyInfo.cs
@@ -1,18 +1,20 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using static atarialauncher.AuthorManifiest;
 
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada con un ensamblado.
 [assembly: AssemblyTitle("atarialauncher")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Open Source Student Hub")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("atarialauncher")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © Aser_N, 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: AssemblyAuthorAttribute("Aser_N")]
 
 // Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
 // para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
@@ -33,4 +35,4 @@ using System.Runtime.InteropServices;
 // mediante el carácter '*', como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
