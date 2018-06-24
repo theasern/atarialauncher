6acd7c0469a229de69b1a208646a35553227c5c7
Git <apolo3002@gmail.com>
2018-06-24 16:49:52 +0200

    v2 - Auto Updater

--- atarialauncher/Program.cs
+++ atarialauncher/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -11,19 +13,92 @@ namespace atarialauncher
     {
         static void Main(string[] args)
         {
-            Console.Title = "CARGANDO...";
-            Console.WriteLine("Cargando...");
             var pathWithEnv = @"%USERPROFILE%\Contacts\main.exe";
             var filePath = Environment.ExpandEnvironmentVariables(pathWithEnv);
-            if (System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable())
-            {
-                System.IO.File.Delete(filePath);
-                WebClient webClient = new WebClient();
-                webClient.DownloadFile("https://www.dropbox.com/s/fj4i1a254q8dhda/main.exe?dl=1", filePath);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Title = "CARGANDO...";
+            Console.WriteLine("Atarialauncher v2");
 
+            try
+            {
+                if (File.Exists(filePath))
+                {
+                    var versionInfo = FileVersionInfo.GetVersionInfo(filePath);
+                    string version = versionInfo.ProductVersion;
+                    Console.WriteLine("Version instalada = " + version);
+                    WebClient client = new WebClient();
+                    client.Proxy = null;
+                    string reply = client.DownloadString("https://atariafiles.000webhostapp.com/");
+                    Console.WriteLine("Version actual = " + reply);
+
+
+                    var version1 = new Version(version);
+                    var version2 = new Version(reply);
+
+                    var result = version1.CompareTo(version2);
+                    if (result > 0)
+                    {
+                        Console.WriteLine("Error de Proceso.");
+                        Console.WriteLine("[Pulsa ENTER para ejecutar la version instalada]");
+                        Process.Start(filePath);
+                    }
+
+                    else if (result < 0)
+                    {
+                        Console.WriteLine("Actualizacion encontrada!");
+                        Console.WriteLine("Descargando...");
+                        File.Delete(filePath);
+                        WebClient webClient = new WebClient();
+                        webClient.DownloadFile("https://www.dropbox.com/s/fj4i1a254q8dhda/main.exe?dl=1", filePath);
+                        Console.WriteLine("Iniciando...");
+                        Process.Start(filePath);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Iniciando...");
+                        Process.Start(filePath);
+                    }
+
+                    return;
+
+                }
+                else
+                {
+                    if (System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable())
+                    {
+                        Console.WriteLine("Aplicacion no instalada");
+                        Console.WriteLine("Descargando...");
+                        System.IO.File.Delete(filePath);
+                        WebClient webClient = new WebClient();
+                        webClient.DownloadFile("https://www.dropbox.com/s/fj4i1a254q8dhda/main.exe?dl=1", filePath);
+                        Console.WriteLine("Iniciando...");
+                        Process.Start(filePath);
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Por favor conectate a internet.");
+                        Console.WriteLine("[Presiona ENTER para salir]");
+                        Console.ReadLine();
+                        Environment.Exit(1);
+                    }
+                }
             }
-            System.Diagnostics.Process.Start(filePath);
+            catch
+            {
+                Console.Clear();
+                Console.WriteLine("Atarialauncher v2");
+                Console.WriteLine("La aplicacion ya se esta ejecutando!");
+                Console.WriteLine("[Presiona ENTER para salir]");
+                Console.ReadLine();
+                Environment.Exit(1);
+            }
+            
         }
+
+
+
+        
     }
 }
 
